5a4370e57c45169d97ce110ec3240386ec4d7361
Shalaginov-VD <kuzia2pet@gmail.com>
2025-03-18 16:46:46 +0500

    created views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Post, Comment
+from .forms import CommentForm
+from django.core.paginator import Paginator
 
-# Create your views here.
+def post_list(request):
+    posts = Post.objects.all()
+    paginator = Paginator(posts, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj})
+
+def post_detail(request, post_id):
+    post = get_object_or_404(Post, id=post_id)
+    comments = post.comments.all()
+    if request.method == 'POST':
+        form = CommentForm(request.POST)
+        if form.is_valid():
+            comment = form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return redirect('post_detail', post_id=post.id)
+        else:
+            form = CommentForm()
+        return render(request, 'blog/post_detail.html', {'post': post, 'comments': comments, 'form': form})
