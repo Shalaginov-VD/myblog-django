0e266dbd16dc9cb0c64b80846f95720b92b36725
Shalaginov-VD <kuzia2pet@gmail.com>
2025-03-18 16:35:02 +0500

    set up the admin panel

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Post, Comment, Category
 
-# Register your models here.
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+class PostAdmin(admin.ModelAdmin):
+    inlines = [CommentInline]
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Comment)
+admin.site.register(Category)
