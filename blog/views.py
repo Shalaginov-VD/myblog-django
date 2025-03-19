4e5b9aec0607468171625eafbd9b9d2d09379aeb
Shalaginov-VD <kuzia2pet@gmail.com>
2025-03-19 16:55:19 +0500

    added filtering by category

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,21 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Post, Comment
+from .models import Post, Comment, Category
 from .forms import CommentForm
 from django.core.paginator import Paginator
 
 def post_list(request):
     query = request.GET.get('q')
+    category_id = request.GET.get('category')
     posts = Post.objects.all()
+    if category_id:
+        posts = posts.filter(category_id=category_id)
     if query:
         posts = posts.filter(title__icontains=query)
     paginator = Paginator(posts, 5)
     page_number = request.GET.get('page')
     page_obj = paginator.get_page(page_number)
-    return render(request, 'blog/post_list.html', {'page_obj': page_obj, 'query': query})
+    categories = Category.objects.all()
+    return render(request, 'blog/post_list.html', {'page_obj': page_obj, 'query': query, 'categories': categories})
 
 def post_detail(request, post_id):
     post = get_object_or_404(Post, id=post_id)
